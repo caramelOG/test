e55145e225531a550e01b6c7f8c810b2b0f6e58f
caramelOG <52350246+caramelOG@users.noreply.github.com>
2019-09-14 19:29:06 +0700

    Add files via upload

---
+++ Test1.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # 1.
+
+# In[ ]:
+
+
+import math
+a = int (input())
+b= int (543)
+area = a-b
+print(area)
+
+
+# # 2.
+
+# In[ ]:
+
+
+print ("Welcome to Earth Shop")
+print ("เลย์                           1 ชิ้น  20 THB","รวม",1*20,"THB")
+print ("น้ําดื่มสิงห์ 150 มล.       1 ชึ้น   7 THB","รวม",1*7,"THB")
+print ("อิชิตันกรีนที ต้นตำหรับ   1 ชึ้น  20 THB","รวม",2*20,"THB")
+print ("ยอดสุทธิ                     3 ชิ้น"        ,   20+7+40,"THB")
+print ("--------------------------------------------------")
+print ("Good Bye")
+
+
+# # 3.
+
+# In[ ]:
+
+
+number = int(input("2:"))
+for i in range(1,13):
+    result = number*i
+    print("%d x %d = %d"%(number,i,result))
+
+
+# # 4.
+
+# In[ ]:
+
+
+
+
